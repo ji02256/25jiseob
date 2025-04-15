0fdab454b8a50b13ad885a4ca5c647655d06448f
ji02256 <js9328736@gmail.com>
2025-04-15 14:17:30 +0900

    20251409
    test

---
+++ MidtermTest/20251409/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("       /\\       \n");
+    printf("      /  \\      \n");
+    printf("     /____\\     \n");
+    printf("    |      |    \n");
+    printf("    |  []  |    \n");
+    printf("    |______|    \n");
+    return 0;
+}
